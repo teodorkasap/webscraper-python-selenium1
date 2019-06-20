01a5f870e3727e5f7dadc6ac78d5dce9bf2e607c
teodorkasap <erol.erten@gmail.com>
2019-06-20 17:13:05 +0300

    implemented - defensive coding for error cases - StaleElementReferenceException handling

--- scraper-single-page-table.py
+++ scraper-single-page-table.py
@@ -1,11 +1,15 @@
 # imports
 from selenium import webdriver
 import pandas as pd
+import numpy as np
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
+from selenium.common.exceptions import StaleElementReferenceException
+
 import time
 import re
+import os
 
 
 # driver - browser
@@ -19,7 +23,7 @@ url2 = 'https://www.ultimatetennisstatistics.com/playerProfile?playerId='
 driver.get(url)
 
 # wait
-wait = WebDriverWait(driver, 10)
+wait = WebDriverWait(driver, 60)
 
 # click the button "20" to show dropdown options to display records per page
 item = wait.until(EC.visibility_of_element_located(
@@ -55,8 +59,8 @@ print(player_ids)
 # pause for table to be loaded
 driver.implicitly_wait(0.5)
 
-# loop over the list of player ids 
-for i in range(0,1):
+# loop over the list of player ids, define range first
+for i in range(400,500):
 
     # compose url's for player detail pages and open these url's
     driver.get(url2+player_ids[i])
@@ -65,7 +69,11 @@ for i in range(0,1):
     item = wait.until(EC.visibility_of_element_located(
     (By.XPATH, "//h3")))
     player_name=item.text
-    print(player_name)
+    print(' ')
+    print('****************')
+    print(player_name) 
+    print(' ')
+    print('****************')
 
     # open dropdown for statistics options
     item = wait.until(EC.visibility_of_element_located(
@@ -99,7 +107,15 @@ for i in range(0,1):
     player_data = []
     # scrape the table with player stats, as text first, then append to list as "list of lists"
     for table in driver.find_elements_by_xpath('//*[contains(@id,"playerStatsTab")]//tr'):
-        data = [item.text for item in table.find_elements_by_xpath(".//*[self::td or self::th]")]
+        driver.implicitly_wait(0.5)
+        try:
+            data = [item.text for item in table.find_elements_by_xpath(".//*[self::td or self::th]")]
+        except StaleElementReferenceException:
+            pass
+
+    # for table in wait.until(EC.visibility_of_all_elements_located((By.XPATH,'//*[contains(@id,"playerStatsTab")]//tr'))):
+    #     data = [item.text for item in table.find_elements_by_xpath(".//*[self::td or self::th]")]
+        
         player_data.append(data)
     
     print(player_data)
@@ -110,25 +126,35 @@ for i in range(0,1):
     print(' ')
     print(data_frame)
 
-    data_frame=data_frame.iloc[[3,6,8,13,14],[0,1]]
-    print(' ')
-    print(' ')
-    print(data_frame)
+    if not data_frame.empty:
+        data_frame=data_frame.iloc[[3,6,8,13,14],[0,1]]
+        print(' ')
+        print(' ')
+        print(data_frame)
 
-    data_frame = data_frame.T
-    columns = ['1st Serve' ,  'Break Points Saved' ,  'Service Games Won' ,  '2nd Srv. Return Won' ,  'Break Points Won' ]
-    data_frame.columns=columns
-    data_frame=data_frame.drop(data_frame.index[0:1])
-    # Change all dtypes to float for calculations in later stages
-    for col in columns:
-        data_frame[col]=data_frame[col].str.replace('%','')
-        data_frame[col]=data_frame[col].astype(float)
+        data_frame = data_frame.T
+        columns = ['1st Serve' ,  'Break Points Saved' ,  'Service Games Won' ,  '2nd Srv. Return Won' ,  'Break Points Won' ]
+        data_frame.columns=columns
+        data_frame=data_frame.drop(data_frame.index[0:1])
+        # Change all dtypes to float for calculations in later stages
+        for col in columns:
+            data_frame[col]=data_frame[col].str.replace('%','')
+            data_frame[col]=data_frame[col].astype(float, errors='ignore')
+        
     
-    data_frame.insert(0,'Player Name',player_name)
-    print(' ')
-    print(' ')
-    print(data_frame)
-    print(data_frame.dtypes)
-    players_master_dataframe_list.append(data_frame)
+        data_frame.insert(0,'Player Name',player_name)
+        print(' ')
+        print(' ')
+        print(data_frame)
+        print(data_frame.dtypes)
+        players_master_dataframe_list.append(data_frame)
 
 print(players_master_dataframe_list)
+players_master_dataframe=pd.concat(players_master_dataframe_list)
+
+filename = 'player_stats.txt'
+
+if not os.path.isfile(filename):
+   players_master_dataframe.to_csv(filename, sep=' ',index=False)
+else: # else it exists so append without writing the header
+   players_master_dataframe.to_csv(filename, mode='a', header=False, sep=' ',index=False)
